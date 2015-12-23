95bf3e44cca41ab4bf569262ad3ae0f756732d1d
Shashi <reddy279@gmail.com>
2015-12-22 21:11:00 -0500

    Added File System Access Code

--- src/main/scala-2.10/com/scala/spark/util/ScalaUtilities.scala
+++ src/main/scala-2.10/com/scala/spark/util/ScalaUtilities.scala
@@ -2,37 +2,83 @@ package com.scala.spark.util
 
 import java.net.URI
 
-
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.fs.{Path, FileSystem}
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.log4j.Logger
 
 
 object ScalaUtilities {
 
-val logger = Logger.getLogger(this.getClass)
+  val logger = Logger.getLogger(this.getClass)
 
   /**
    * This function renturns FileSystem Object
    * @return
    */
 
-def getFileSystemObject(file_location:String): FileSystem ={
+  def getFileSystem(file_location: String, file_system_type: String): FileSystem = {
 
-  val configuration:Configuration =new Configuration()
-    logger.info("File Syste, Location " + file_location)
-  FileSystem.get(new URI(file_location), configuration)
-}
+    val configuration: Configuration = new Configuration()
+    var file_system: FileSystem = null
+    if (file_system_type.equalsIgnoreCase("HDFS")) {
+      logger.info("HDFS File System, Location " + file_location)
+      file_system = FileSystem.get(new URI(file_location), configuration)
+    }
+    if (file_system_type.equalsIgnoreCase("S3")) {
+      logger.info("S3 File System, Location " + file_location)
+      file_system = getAWSFileSystem(file_location)
+    }
+    file_system
+  }
+
+
+  /**
+   * This Function returns the Amazon AWS S3 Files System object
+   */
+  def getAWSFileSystem(file_location: String): FileSystem = {
+
+    val configuration: Configuration = new Configuration()
+    try {
+      logger.info("Reading AWS Environment Variabls from system")
+      println("Reading AWS Environment Variabls from system")
+      val awsAccessKeyId: String = sys.env("AWS_ACCESS_KEY_ID") //"AKIAJ3YUCQDPWH5CXEUQ"
+      val awsSecretAccessKey: String = sys.env("AWS_SECRET_ACCESS_KEY") //"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"
+      println("awsAccessKeyId= " + awsAccessKeyId + "\n " + "awsSecretAccessKey= " + awsSecretAccessKey)
+
+      if (awsAccessKeyId != null && awsSecretAccessKey != null) {
+        logger.info("Setting the AWS keys to Configuration object")
+        configuration.set("fs.s3n.awsAccessKeyId", awsAccessKeyId)
+        configuration.set("fs.s3n.awsSecretAccessKey", awsSecretAccessKey)
+        configuration.set("fs.s3a.awsAccessKeyId", awsAccessKeyId)
+        configuration.set("fs.s3a.awsSecretAccessKey", awsSecretAccessKey)
+        configuration.set("fs.s3.awsAccessKeyId", awsAccessKeyId)
+        configuration.set("fs.s3.awsSecretAccessKey", awsSecretAccessKey)
+      }
+    } catch {
+      case e: Exception => logger.info("AWS keys were not found...")
+    }
+    configuration.set("fs.s3.impl", "org.apache.hadoop.fs.s3native.NativeS3FileSystem")
+    configuration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+    configuration.set("fs.s3a.server-side-encryption-algorithm", "AES256")
+
+    FileSystem.get(new URI(file_location), configuration)
+
+
+  }
+
 
   /**
    * This method verifies whether the given file exists or not
+   * path must be either your hdfs location  ex:hdfs://localhost:9000/.... or S3 location
    * @param file_location
    * @return
    */
 
- def isFileSystemFileExist(file_location:String):Boolean ={
-    val fileSystem:FileSystem  = getFileSystemObject(file_location)
-    val path:Path = new Path(file_location)
-     fileSystem.exists(path)
- }
+  def isFileSystemFileExist(file_location: String, file_system_type: String): Boolean = {
+    val fileSystem: FileSystem = getFileSystem(file_location, file_system_type)
+    val path: Path = new Path(file_location)
+    fileSystem.exists(path)
+  }
+
+
 }
--- src/test/scala-2.10/com/scala/spark/util/test/ScalaUtilitiesTest.scala
+++ src/test/scala-2.10/com/scala/spark/util/test/ScalaUtilitiesTest.scala
@@ -1,9 +1,7 @@
 package com.scala.spark.util.test
 
 import com.scala.spark.util.ScalaUtilities
-import org.apache.hadoop.fs
-import org.apache.hadoop.fs.FileSystem
-import org.scalatest.{BeforeAndAfter, Matchers, FlatSpec}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 class ScalaUtilitiesTest extends FlatSpec with Matchers with BeforeAndAfter{
 
@@ -15,11 +13,20 @@ class ScalaUtilitiesTest extends FlatSpec with Matchers with BeforeAndAfter{
   }
 
   /**
-   *  A test case to test the file system connection and file existance
+   *  A test case to test the HDFS file system connection and file existance
    */
 
-   "The following method" should  " return File System object "  in {
-      val returnObject:Boolean = ScalaUtilities.isFileSystemFileExist("hdfs://localhost:9000/user/shashidharsreddy/RampUp/CurrSmallCSV.csv")
+   "The following method" should  " check if the HDFS File System file exists "  in {
+      val returnObject:Boolean = ScalaUtilities.isFileSystemFileExist("hdfs://localhost:9000/user/shashidharsreddy/RampUp/CurrSmallCSV.csv","hdfs")
      returnObject shouldBe  true
    }
+
+  /**
+   *  A test case to test the AWS S3 file system connection and file existance
+   */
+
+  "The following method" should  " check if the AWS S3 File System file exists "  in {
+    val returnObject:Boolean = ScalaUtilities.isFileSystemFileExist("s3://shashi-s3/data/shashi.txt","s3")
+    returnObject shouldBe  true
+  }
 }
